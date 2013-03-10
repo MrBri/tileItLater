389fe327d4a89149e17f052581106c2686d66858
MrBri <ty8bri@gmail.com>
2013-03-10 10:19:26 -0700

    Styling and add isotope. Isotope also working so far.

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ var urlCtrl = function($scope) {
 	$scope.master = [];
 	$scope.setRmd = function(milli){
 		$scope.tile.remind = Date.now() + milli;
+		$scope.master.push(angular.copy($scope.tile));
+		$('.tiles').isotope( 'insert', $scope.master.slice(-1) );
 	};
 };
+
+// $('.tiles').isotope({
+// 	itemSelector: '.tile'
+// });
