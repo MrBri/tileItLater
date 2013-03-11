c6deb9ab1766e5314b5f4073d50053e25e12fab8
MrBri <ty8bri@gmail.com>
2013-03-11 15:58:07 -0700

    Integrate and working socket.io. On my way to getting some screencapturs.

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,19 @@
+// angular.module('urlApp', [])
+// 	.directive('isotope', function() {
+// 		return function(scope,element,attrs){
+// 			element.isotope({itemSelector:'.tile', resizesContainer:false});
+// 			scope.$watch(attrs.ngModel, function() {
+// 				element.isotope( 'reloadItems' );
+// 			});
+// 		};
+// 	});
 
 var urlCtrl = function($scope) {
 	$scope.master = [];
 	$scope.setRmd = function(milli){
 		$scope.tile.remind = Date.now() + milli;
 		$scope.master.push(angular.copy($scope.tile));
-		$('.tiles').isotope( 'insert', $scope.master.slice(-1) );
+		// $('.tiles').isotope( 'insert', $scope.master.slice(-1) );
 	};
 };
 
--- web.js
+++ web.js
@@ -1,19 +1,34 @@
-var static = require('node-static');
+// var static = require('node-static');
+var connect = require('connect'),
+    socketio = require('socket.io');
+
+var port = process.env.PORT || 3000;
+var server = connect(
+  connect.static(__dirname + '/app')
+).listen(port);
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function(client){
+	console.log('Client connected...');
+
+	client.emit('message', {hello: 'World'});
+});
+
 
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new(static.Server)('./app');
-var port = process.env.PORT || 3000;
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-	file.serve(request, response, function(err, result){
-		if(err && (err.status === 404)){
-		        file.serveFile('/index.html', 200, {}, request, response);
-		}
-	});
-    });
-}).listen(port);
+// var file = new(static.Server)('./app');
+// var port = process.env.PORT || 3000;
+// require('http').createServer(function (request, response) {
+//     request.addListener('end', function () {
+//         //
+//         // Serve files!
+//         //
+// 	file.serve(request, response, function(err, result){
+// 		if(err && (err.status === 404)){
+// 		        file.serveFile('/index.html', 200, {}, request, response);
+// 		}
+// 	});
+//     });
+// }).listen(port);
