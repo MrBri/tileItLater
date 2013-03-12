7494172678715c8d1877edc12214d1cdc823adc4
MrBri <ty8bri@gmail.com>
2013-03-11 19:34:51 -0700

    Capturing via asynchronous html input.

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
-// var static = require('node-static');
-var webshot = require('webshot'),
+var fs = require('fs'),
+		webshot = require('webshot'),
 		connect = require('connect'),
     socketio = require('socket.io');
 
@@ -8,18 +8,17 @@ var server = connect(
   connect.static(__dirname + '/app')
 ).listen(port);
 
-var webshotOptions = {
-  screenSize: {
-    width: 320,
-		height: 480
-  },
-  shotSize: {
-    width: 320,
-		height: '320'
-  },
-  userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
-    + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
-};
+// var webshotOptions = {
+//   screenSize: {
+//     width: 320,
+// 		height: 480
+//   },
+//   shotSize: {
+//     width: 320,
+// 		height: '320'
+//   },
+//   userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+// };
 
 var io = socketio.listen(server);
 io.sockets.on('connection', function(client){
@@ -29,38 +28,14 @@ io.sockets.on('connection', function(client){
 
 	client.on('capture', function (url) {
 		console.log("On web.js", url);
-		webshot(url, url + '.png', webshotOptions, function(err) {
-			if ( err ) {
-				throw err;
-			}
-			console.log('Taking webshot..');
+		webshot(url, function(err, renderStream) {
+			if (err) throw err;
+
+			var file = fs.createWriteStream('./img/' + url + '.png', {encoding: 'binary'});
+
+			renderStream.on('data', function(data) {
+				file.write(data.toString('binary'), 'binary');
+			});
 		});
 	});
 });
-
-
-// webshot('google.com', 'google.png', options, function(err){
-// 	if ( err ) {
-// 		throw err;
-// 	}
-// 	console.log('Taking webshot..');
-// });
-
-
-//
-// Create a node-static server instance to serve the './public' folder
-//
-// var file = new(static.Server)('./app');
-// var port = process.env.PORT || 3000;
-// require('http').createServer(function (request, response) {
-//     request.addListener('end', function () {
-//         //
-//         // Serve files!
-//         //
-// 	file.serve(request, response, function(err, result){
-// 		if(err && (err.status === 404)){
-// 		        file.serveFile('/index.html', 200, {}, request, response);
-// 		}
-// 	});
-//     });
-// }).listen(port);
